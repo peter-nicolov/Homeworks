05de7bd48e85534cca14b5972bae72403711c7e9
Peter Nicolov <pnico@softserveinc.com>
2022-05-31 11:26:48 +0300

    ForLoopReverse edit

--- src/Demo/ForLoopReverse.java
+++ src/Demo/ForLoopReverse.java
@@ -5,11 +5,7 @@ public class ForLoopReverse {
         public static void main(String[] args){
 
             int arr[]={10, 20, 30, 40, 50};
-
-            for(int i=4; i < arr.length; i--){
-                if (i<0){
-                    break;
-                }
+            for(int i=arr.length-1; i>=0; i--){
                 System.out.println(arr[i]);
             }
         }
